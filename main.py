e726eb4109788127784912d7df7603c81a2c5823
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-21 19:41:33 +0500

    I created new file called main.py in order to run the whole project.

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+main.py
+~~~~~~~
+
+this file will  help me to run the whole project
+using files inside of src folder, using the data
+from data folder.
+"""
+
+def run_main():
+    print("Assalamu alaykum dunya")
+
+
+if __name__ == "__main__":
+    run_main()
