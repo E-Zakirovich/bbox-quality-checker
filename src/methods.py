a27d1838b9a33d0446be9b5701e82e3e64a62187
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-21 19:37:39 +0500

    class is initialized

--- src/methods.py
+++ src/methods.py
@@ -12,4 +12,21 @@ from pathlib import Path
 
 class Methods:
 
-    ...
+    def __init__(self, 
+                 weights_path: str, 
+                 images_path : str, 
+                 labels_path : str, 
+                 predictions_images_path : str, 
+                 predictions_labels_path : str,
+                 comparisons_path : str,
+                 statistics_path : str
+
+                 ):
+        # path settings
+        self.weights_path = weights_path
+        self.images_path = images_path
+        self.labels_path = labels_path
+        self.predictions_images_path = predictions_images_path
+        self.predictions_labels_path = predictions_labels_path
+        self.comparisons_path = comparisons_path
+        self.statistics_path = statistics_path
