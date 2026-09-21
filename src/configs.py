fe38832e44130a08c17559213f55be53c7d2c7f0
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-21 18:52:07 +0500

    I stored path settings.

--- src/configs.py
+++ src/configs.py
@@ -4,3 +4,11 @@ configs.py
 This file will help me to get static settings
 for other fils to usage.
 """
+
+# path settings
+weights_path = "./weights/best.pt"
+images_path = "./data/images"
+labels_path = "./data/labels"
+predictions_path = "./outputs/predictions"
+comparisons_path = "./outputs/comparisons"
+statistics_path = "./outputs/predictions"
