1710bfe6b17944203a69294d46dfe80309913004
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-21 19:37:58 +0500

    I deleted all not useful files.

--- src/predictions.py
+++
@@ -1,8 +0,0 @@
-"""
-predictions.py
-~~~~~~~~~~~~~~~
-
-This file will  help  me to predict images,  using 
-weights that I trained last month(August 17, 2026)
-"""
-
--- src/utils.py
+++
@@ -1,8 +0,0 @@
-"""
-methods.py
-~~~~~~~~~~~
-
-inside of this file, I will store all useful methods that 
-I will use in predictions.py file in order to predict the
-data to find IoU.
-"""
