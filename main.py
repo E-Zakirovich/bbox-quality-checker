b5023359d6dee6c0e4788f46e7532824c77fb27f
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-21 20:52:52 +0500

    testing part of new two methods successfull.

--- main.py
+++ main.py
@@ -6,9 +6,23 @@ this file will  help me to run the whole project
 using files inside of src folder, using the data
 from data folder.
 """
+from src.methods import Methods
+import src.configs as get
+
+method = Methods(get.weights_path, get.images_path, get.labels_path, get.predictions_images_path, get.predictions_labels_path, get.comparisons_path, get.statistics_path)
 
 def run_main():
-    print("Assalamu alaykum dunya")
+
+    d = method.get_labels(
+        get.labels_path
+    )
+
+
+
+    txt = method.read_txt(d)
+
+    for i in txt:
+        print(i)
 
 
 if __name__ == "__main__":
