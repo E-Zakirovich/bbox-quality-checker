4511505774613161af1e90555538e3ec9efd8b58
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-21 11:26:41 +0500

    I changed the name of utils.py file to methods.py file. The reason is - I just hate it. Thats it.

---
+++ src/methods.py
@@ -0,0 +1,15 @@
+"""
+methods.py
+~~~~~~~~~~~
+
+inside of this file, I will store all useful methods that 
+I will use in predictions.py file in order to predict the
+data to find IoU.
+"""
+
+# import libraries 
+from pathlib import Path
+
+class Methods:
+
+    ...
