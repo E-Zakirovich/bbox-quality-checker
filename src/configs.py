1f6351c3ba4a4a3f04c9cc8bd1a1f5d034df001b
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-22 12:40:05 +0500

    I added yolo settings

--- src/configs.py
+++ src/configs.py
@@ -13,3 +13,7 @@ predictions_images_path = "./outputs/predictions/images"
 predictions_labels_path = "./outputs/predictions/labels"
 comparisons_path = "./outputs/comparisons"
 statistics_path = "./outputs/predictions"
+
+# YOLO model params
+conf = 0.25 
+verbose = False
