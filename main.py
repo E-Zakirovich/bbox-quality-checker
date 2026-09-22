0f6afa83fa74d06289bbe28c90db563df3d0e2ad
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-22 12:41:21 +0500

    i tested model runner, it is working perfectly and all results stores inside of outputs/predictions/labels folder

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from data folder.
 """
 from src.methods import Methods
 import src.configs as get
+from ultralytics import YOLO
 
 method = Methods(get.weights_path, get.images_path, get.labels_path, get.predictions_images_path, get.predictions_labels_path, get.comparisons_path, get.statistics_path)
 
@@ -18,7 +19,9 @@ def run_main():
         "variables" : [1, 2, 3, 4]
     }
 
-    method.write(data, get.predictions_labels_path, "3.txt")
+    model = YOLO(get.weights_path)
+
+    method.run_the_model(model)
 
 
 if __name__ == "__main__":
